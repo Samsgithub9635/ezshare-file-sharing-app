08992dbb8c54b8e1a6cdbd3dbb45b40bce59757a
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-07-15 19:48:55 +0530

    Integrate FileService in controller to render all files on index route

--- src/main/java/com/ezshare/controller/filecontroller.java
+++ src/main/java/com/ezshare/controller/filecontroller.java
@@ -2,14 +2,30 @@ package com.ezshare.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ezshare.service.FileService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 
 @Controller //controller returns html file as per returned 
 @RequestMapping() // added files as the end point for controller
-public class filecontroller {
+public class Filecontroller {
+
+    @Autowired
+    private FileService fileService;
+
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("files", fileService.getAll());
+        return new String();
+    }
+    
     @GetMapping("/home")
     public String login() {
         return "home"; //return home.html
