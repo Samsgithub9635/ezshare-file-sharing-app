53d895e038f88744d3e605a6620002b6baae9048
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-07-18 23:04:18 +0530

    changed method name findByDateBefore to findByTimeBefore at L13

--- src/main/java/com/ezshare/repository/FileRepository.java
+++ src/main/java/com/ezshare/repository/FileRepository.java
@@ -10,7 +10,7 @@ import com.ezshare.entity.FileEntity;
 
 @Repository
 public interface FileRepository extends JpaRepository<FileEntity, Integer> {
-    List<FileEntity> findByExpiryDateBefore(LocalDateTime now); // used to expire the data after 24hrs of uploading
+    List<FileEntity> findByExpiryTimeBefore(LocalDateTime now); // used to expire the data after 24hrs of uploading
 
     
 
