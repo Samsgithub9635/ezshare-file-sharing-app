f4ed8b8d6e541dc1257bf5f4bd7084851cc98d98
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-07-18 23:16:51 +0530

    Refactored shareFile and upload logic; fixed crash due to invalid repository field

--- src/main/java/com/ezshare/service/FileServiceImpl.java
+++ src/main/java/com/ezshare/service/FileServiceImpl.java
@@ -1,12 +1,9 @@
 package com.ezshare.service;
 
-
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
-
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,62 +12,67 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ezshare.entity.FileEntity;
+import com.ezshare.exception.FileNotFoundException;
 import com.ezshare.model.FileModel;
 import com.ezshare.repository.FileRepository;
-import com.ezshare.exception.FileNotFoundException;
 
 @Service
-public class FileServiceImpl implements FileService{
+public class FileServiceImpl implements FileService {
 
-    @Autowired  //dependency injection
+    @Autowired
     private FileRepository fileRepository;
 
+    // Utility method to convert Entity -> Model
     private FileModel convertToModel(FileEntity entity) {
         FileModel model = new FileModel();
         BeanUtils.copyProperties(entity, model);
         return model;
     }
 
-    @Override
-    public List<FileModel> getAll() {
-        
-        List<FileEntity> entities = fileRepository.findAll();
-        return entities.stream().map(this:: convertToModel).collect(Collectors.toList());
-    }
-
-    @Override
-    public ResponseEntity<?> uploadFile(MultipartFile file, String uploadedby) throws IOException {
-        
+    // Utility method to convert MultipartFile -> Entity
+    private FileEntity convertToEntity(MultipartFile file, String uploadedBy) throws IOException {
         FileEntity entity = new FileEntity();
         entity.setFilename(file.getOriginalFilename());
-        entity.setUploadedBy(uploadedby);
+        entity.setFileData(file.getBytes());
+        entity.setUploadedBy(uploadedBy);
         entity.setUploadTime(LocalDateTime.now());
         entity.setExpiryTime(LocalDateTime.now().plusDays(1));
-        entity.setFileData(file.getBytes());
+        return entity;
+    }
+
+    // Upload a file and save to DB
+    @Override
+    public ResponseEntity<String> uploadFile(MultipartFile file, String uploadedBy) throws IOException {
+        FileEntity entity = convertToEntity(file, uploadedBy);
         fileRepository.save(entity);
-        return ResponseEntity.ok().body(convertToModel(entity));
+        return ResponseEntity.ok("File uploaded successfully");
     }
 
+    // Delete a file using ID
     @Override
-    public ResponseEntity<?> sharedFile(int id) {
-        Optional<FileEntity> entity =fileRepository.findById(id);
-        if(entity.isPresent()) {
-            return ResponseEntity.ok().body(convertToModel(entity.get()));
-
-        }else{
-            throw new FileNotFoundException("File not Found !!!");
-        }
+    public ResponseEntity<String> deleteFile(int id) {
+        return fileRepository.findById(id)
+            .map(file -> {
+                fileRepository.deleteById(id);
+                return ResponseEntity.ok("Deleted");
+            })
+            .orElseThrow(() -> new FileNotFoundException("File not found with ID: " + id));
     }
 
+    // Get file data to "share" it (as a DTO/model object)
     @Override
-    public ResponseEntity<?> deleteFile(int id) {
-        Optional <FileEntity> entity = fileRepository.findById(id);
-        if(entity.isPresent()){
-            fileRepository.delete(entity.get());
-            return ResponseEntity.ok().body("File Deleted Successfully!");
-        }else{
-            throw new FileNotFoundException("File not Found !!!");
-        }
+    public ResponseEntity<FileModel> shareFile(int id) {
+        return fileRepository.findById(id)
+            .map(file -> ResponseEntity.ok(convertToModel(file)))
+            .orElseThrow(() -> new FileNotFoundException("File not found with ID: " + id));
     }
-    
+
+    // Return all files as DTOs
+    @Override
+    public List<FileModel> getAll() {
+        return fileRepository.findAll()
+                .stream()
+                .map(this::convertToModel)
+                .collect(Collectors.toList());
+    }
 }
